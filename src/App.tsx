c7a27c42dd9b7c8273185b2044e3283a10749ccb
Grace Nieboer <grace.nieboer@outlook.com>
2024-09-03 22:17:04 -0400

    initial commit, react query added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RHF from './pages/RHF';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+
+    <RHF />
+
     </div>
   );
 }
