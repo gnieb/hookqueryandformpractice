87b0e910c18c4f6b9086a884006c13a9180fe3b4
Grace Nieboer <grace.nieboer@outlook.com>
2024-09-03 22:22:45 -0400

    configure tailwind real quick

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import RHF from './pages/RHF';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-    <RHF />
-
+    <div className="flex text-center items-center justify-center">
+      <div>
+        <RHF />
+      </div>
     </div>
   );
 }
